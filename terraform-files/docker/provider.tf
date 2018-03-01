d983611507eabf5200308d8d2d2d6dcb6bd71578
Ted Wood <ted@xy0.org>
2018-02-28 20:43:04 -0500

    Switch to blackburn theme

---
+++ terraform-files/docker/provider.tf
@@ -0,0 +1,9 @@
+variable "do_token" {}
+variable "pub_key" {}
+variable "pvt_key" {}
+variable "ssh_fingerprint" {}
+variable "code_dir" {}
+
+provider "digitalocean" {
+    token = "${var.do_token}"
+}
