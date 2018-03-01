57a6a73e2b0f01e8f36ba4d566d8c947a8c50e8a
Ted Wood <ted@mailchimp.com>
2018-03-01 11:59:08 -0500

    Removed unused files

--- terraform-files/docker/provider.tf
+++
@@ -1,9 +0,0 @@
-variable "do_token" {}
-variable "pub_key" {}
-variable "pvt_key" {}
-variable "ssh_fingerprint" {}
-variable "code_dir" {}
-
-provider "digitalocean" {
-    token = "${var.do_token}"
-}
